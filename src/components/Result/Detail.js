917daac48e36969eb32ccbf5ef540723921adb70
tmhammer <timh@govwizely.com>
2017-06-16 17:35:41 -0400

    Update fields

--- src/components/Result/Detail.js
+++ src/components/Result/Detail.js
@@ -1,45 +1,44 @@
 import React, { PropTypes } from 'react';
 import { AddressList, IdentificationList, Link, Row, UnorderedList } from './DetailItem';
+import moment from 'moment';
 
 const Detail = ({ result }) => (
   <table className="explorer__result-item__detail">
     <tbody>
-      <Row label="Company">{result.company}</Row>
-      <Row label="Registration Number">{result.registration_number}</Row>
-      <Row label="Username">{result.username}</Row>
+      <Row label="Organization">{result.company}</Row>
       <Row label="Address">{result.address}</Row>
       <Row label="City">{result.city}</Row>
       <Row label="State Name">{result.state_name}</Row>
-      <Row label="ZIP">{result.zip}</Row>
+      <Row label="Zip Code">{result.zip}</Row>
       <Row label="Phone">{result.phone}</Row>
       <Row label="Fax">{result.fax}</Row>
       <Row label="Website">
         <Link value={result.website} />
       </Row>
 
-      <Row label="Contact Office">{result.contact_office}</Row>
-      <Row label="Contact Name">{result.contact_name}</Row>
-      <Row label="Contact Title">{result.contact_title}</Row>
-      <Row label="Contact Phone">{result.contact_phone}</Row>
-      <Row label="Contact Fax">{result.contact_fax}</Row>
-      <Row label="Contact Email">{result.contact_email}</Row>
+      <Row label="Organization Contact Office">{result.contact_office}</Row>
+      <Row label="Organization Contact Name">{result.contact_name}</Row>
+      <Row label="Organization Contact Title">{result.contact_title}</Row>
+      <Row label="Organization Contact Phone">{result.contact_phone}</Row>
+      <Row label="Organization Contact Fax">{result.contact_fax}</Row>
+      <Row label="Organization Contact Email">{result.contact_email}</Row>
 
-      <Row label="Corporation Officer">{result.corp_officer}</Row>
-      <Row label="Corporation Officer Title">{result.corp_officer_title}</Row>
-      <Row label="Corporation Officer Phone">{result.corp_officer_phone}</Row>
-      <Row label="Corporation Officer Fax">{result.corp_officer_fax}</Row>
-      <Row label="Corporation Officer Email">{result.corp_officer_email}</Row>
+      <Row label="Corporate Officer">{result.corp_officer}</Row>
+      <Row label="Corporate Officer Title">{result.corp_officer_title}</Row>
+      <Row label="Corporate Officer Phone">{result.corp_officer_phone}</Row>
+      <Row label="Corporate Officer Fax">{result.corp_officer_fax}</Row>
+      <Row label="Corporate Officer Email">{result.corp_officer_email}</Row>
 
-      <Row label="Privacy Policy Effective Date">{result.pp_effective_date}</Row>
+      <Row label="Privacy Policy Effective Date">{moment(result.pp_effective_date).format('MM-DD-YYYY')}</Row>
       <Row label="Privacy Policy Location">
         <Link value={result.pp_location} />
       </Row>
-      <Row label="Statutory Description">{result.statutory_desc}</Row>
+      <Row label="Regulated By">{result.statutory_desc}</Row>
       <Row label="Verification Method">{result.verification_method}</Row>
-      <Row label="Personal Data">{result.personal_data}</Row>
-      <Row label="HR Data">{result.hr_data}</Row>
-      <Row label="EU Data Protection">{result.eu_data_protection}</Row>
-      <Row label="EU Countries">
+      <Row label="Personal Data Covered">{result.personal_data}</Row>
+      <Row label="Organization Human Resources Data Covered">{result.hr_data}</Row>
+      <Row label="Agreement to Cooperate and Comply with EU and/or Swiss Data Protection Authorities">{result.eu_data_protection}</Row>
+      <Row label="Relevant Countries from which Personal Information Received">
         <UnorderedList value={result.eu_countries} />
       </Row>
 
@@ -48,15 +47,8 @@ const Detail = ({ result }) => (
       <Row label="Industry Sector 3">{result.industry_sector_3}</Row>
       <Row label="Industry Sector 4">{result.industry_sector_4}</Row>
 
-      <Row label="Sales Amount">{result.sales_amount}</Row>
-      <Row label="Emp Range">{result.emp_range}</Row>
-      <Row label="Status">{result.status}</Row>
-      <Row label="Certification Status Description">{result.cert_status_desc}</Row>
-      <Row label="Certification Date">{result.certification_date}</Row>
-      <Row label="Annual Renewal Date">{result.annual_renewal_date}</Row>
-      <Row label="EU Certified Through">{result.eu_certified_through}</Row>
-      <Row label="Last Renewed Date">{result.last_renewed_date}</Row>
-      <Row label="Next Notification Date">{result.next_notification_date}</Row>
+      <Row label="Original Certification Date">{moment(result.certification_date).format('MM-DD-YYYY')}</Row>
+      <Row label="EU Certified Through">{moment(result.eu_certified_through).format('MM-DD-YYYY')}</Row>
     </tbody>
   </table>
 );
