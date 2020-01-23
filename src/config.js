56022bf2e0948b26979952eaa4ce64ad661fda1a
Donna Hogan <ddhogan@users.noreply.github.com>
2020-01-23 07:52:00 -0500

    [ODP-763] Migrate to access token

--- src/config.js
+++ src/config.js
@@ -4,16 +4,16 @@ const config = assign({
   development: {
     api: {
       safe_harbor: {
-        host: 'https://api.trade.gov/v1/safe_harbor_eu/search',
-        apiKey: 'O6fmOIPtrvDlqoDe2_6UbKJc',
+        host: 'https://api.trade.gov/gateway/v1/safe_harbor_eu/search',
+        access_token: 'b0045391-2ef8-3049-a215-f78b7716f045',
       },
     },
   },
   production: {
     api: {
       safe_harbor: {
-        host: 'https://api.trade.gov/v1/safe_harbor_eu/search',
-        apiKey: 'O6fmOIPtrvDlqoDe2_6UbKJc',
+        host: 'https://api.trade.gov/gateway/v1/safe_harbor_eu/search',
+        access_token: 'b0045391-2ef8-3049-a215-f78b7716f045',
       },
     },
   },
